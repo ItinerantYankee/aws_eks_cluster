bf1f435edfa44821dac901f41b1d49458b044aa4
itinerantyankee <itinerantyankee@gmail.com>
2025-01-29 15:22:55 -0800

    main.tf now has 6 subnets and an Internet Gateway.

--- main.tf
+++ main.tf
@@ -22,3 +22,73 @@ provider "aws" {
   profile = "default"
 }
 
+# Define cluster name
+variable "cluster_name" {
+  description = "Kubernetes cluster name"
+  type = string
+  default = "eks"
+}
+
+# Define VPC Name
+variable "vpc_name" {
+  description = "Name of VPC"
+  type = string
+}
+
+# Define subnet name prefix
+variable "subnet_prefix" {
+  description = "Prefix for subnet name. Number will be appended."
+  type = string
+  default = "subnet"
+}
+
+# Get availability zones
+data "aws_availability_zones" "available_zones" {}
+
+# Create VPC
+resource "aws_vpc" "eks-vpc" {
+  cidr_block = "192.168.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+# Create subnets with public access
+resource "aws_subnet" "public_subnets" {
+  vpc_id = aws_vpc.eks-vpc.id
+  count = 3
+  cidr_block = "192.168.${count.index + 1}.0/24"
+  availability_zone = data.aws_availability_zones.available_zones.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.subnet_prefix}-${count.index + 1}-public"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"      # Used by AWS to identify this subnet for use by cluster
+    "kubernetes.io/role/elb" = "1"    # Tels AWS Load Balancer Controller to use this subnet for external-facing LBs
+  }
+}
+
+# Create private subnets
+resource "aws_subnet" "private_subnets" {
+  vpc_id = aws_vpc.eks-vpc.id
+  count = 3
+  cidr_block = "192.168.${count.index + 4}.0/24"
+  availability_zone = data.aws_availability_zones.available_zones.names[count.index]
+
+  tags = {
+    Name = "${var.subnet_prefix}-${count.index + 1}"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "eks_internet_gateway" {
+  vpc_id = aws_vpc.eks-vpc.id
+
+  tags = {
+    Name = "EKS-Internet-Gateway"
+  }
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,4 @@
-aws_region = "us-east=1"
+aws_region = "us-east-1"
+vpc_name = "Terraform EKS VPC"
+subnet_prefix = "eks-subnet"
+cluster_name = "terraform-eks"
